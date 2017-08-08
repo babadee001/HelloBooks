33dfbfd0065ca2015e6a1c1f22bafd715c1d76c0
babadee001 <temidayooyedele300@gmail.com>
2017-08-08 20:03:38 +0100

    implement password hashing using bcrypt

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,14 @@
 const User = require('../models').Users;
+const bcrypt = require('bcrypt');
 
 module.exports = {
   create(req, res) {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
     return User
       .create({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
         membership: req.body.membership,
       })
       .then(user => res.status(201).send({
@@ -17,15 +19,17 @@ module.exports = {
       }));
   },
   signin(req, res) {
-    return User
-      .findOne({
-        where: { username: req.body.username, password: req.body.password },
-        attributes: ['username', 'password'],
-      }).then(user => {
-        if (!user) {
-          res.status(404).send('Invalid username or password');
+    return User.findOne(
+      {
+        where: {
+          username: req.body.username,
+        },
+      })
+      .then((user) => {
+        if (user && bcrypt.compareSync(req.body.password, user.password)) {
+          res.status(201).send('Login successful');
         }
-        res.status(200).send('Login successful ');
+        res.status(400).send('Wrong username or password');
       });
   },
 };
