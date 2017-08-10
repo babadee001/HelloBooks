b8b33ee953584237241ac99b9a6bc2670578a8dd
babadee001 <temidayooyedele300@gmail.com>
2017-08-10 10:24:38 +0100

    add start end point

---
+++ bin/www.js
@@ -0,0 +1,9 @@
+import http from 'http';
+import app from '../server/server';
+
+const port = +process.env.PORT || 3000;
+app.set('port', port);
+
+http.createServer(app).listen(port, () => {
+  console.log(`The server is listening on port ${port}`);
+});
