1b32b5bdd9d25c77c89ae5e1dddc236fd7acd430
babadee001 <temidayooyedele300@gmail.com>
2017-08-11 00:50:09 +0100

    modify models index

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+import dbConfig from './config/config.json';
+
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = dbConfig[env];
 const db = {};
-
+dotenv.config();
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -20,13 +21,12 @@ fs
   .filter(file =>
     (file.indexOf('.') !== 0) &&
     (file !== basename) &&
-    (file.slice(-3) === '.js'))
-  .forEach(file => {
+    (file.slice(-3) === '.js')).forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -35,5 +35,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-
-module.exports = db;
+export default db;
