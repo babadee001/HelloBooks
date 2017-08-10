1991e1b79056c9969b3b062d224bc6ab4c2d0872
babadee001 <temidayooyedele300@gmail.com>
2017-08-10 18:33:05 +0100

    remove procfile

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,12 +1,37 @@
-const app = require('../server/server');
-const request = require('supertest');
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import Server from '../bin/www';
 
-describe('GET /', () => {
-  it('responds with a 404 and error message in json', (done) => {
-    request(app)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(404, { message: 'Nothing to see here' }, done);
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe('Test', () => {
+  it('should access api end point', (done) => {
+    chai.request(Server)
+      .get('/api')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
   });
+  it('should not get access to all books without a token', (done) => {
+    chai.request(Server)
+      .get('/api/v1/books')
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
+  });
+  /**it('should signup users', (done) => {
+    chai.request(Server)
+      .get('signup')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({ username: 'test', password: 'testpassword', email: 'testemail' })
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });**/
 });
