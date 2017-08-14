520f26eff131abaaf4241873a3aa8f9075995d16
babadee001 <temidayooyedele300@gmail.com>
2017-08-14 21:27:51 +0100

    add server file for heroku entry point

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import validator from 'express-validator';
-import UserRouter from './server/routes/users';
-import BookRouter from './server/routes/books';
+// import UserRouter from './server/routes/users';
+// import BookRouter from './server/routes/books';
 
 dotenv.load();
 const app = express();
@@ -14,11 +14,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
-app.use('/api/v1/users', UserRouter);
-app.use('/api/v1/books', BookRouter);
+// app.use('/api/v1/users', UserRouter);
+// app.use('/api/v1/books', BookRouter);
 app.get('/api', (req, res) => {
   res.header(200);
   res.send('Welcome to Hello-Books API');
+});
+app.get('/', (req, res) => {
+  res.header(200);
+  res.send('Welcome!!!!');
 });
 
 const port = process.env.PORT || 3000;
