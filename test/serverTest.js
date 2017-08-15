220c3ac5828b9e055a861e25634dbca27b089bed
babadee001 <temidayooyedele300@gmail.com>
2017-08-15 03:31:51 +0100

    add more tests

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,37 +1,65 @@
+import supertest from 'supertest';
 import chai from 'chai';
-import chaiHttp from 'chai-http';
-import Server from '../server';
+import mocha from 'mocha';
+import app from '../server';
+import models from '../server/models/';
+import seeder from '../server/seeders/index';
 
+const server = supertest.agent(app);
 const should = chai.should();
 
-chai.use(chaiHttp);
 
+before((done) => {
+  models.sequelize.sync({ force: true }).then(() => {
+    done(null);
+  }).catch((errors) => {
+    done(errors);
+  });
+});
 
-describe('Test', () => {
-  it('should access api end point', (done) => {
-    chai.request(Server)
-      .get('/api')
+describe('User Registration', () => {
+  it('allows a new user to register', (done) => {
+    server
+      .post('/api/v1/users/signup')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(seeder.signUp)
+      .expect(201)
       .end((err, res) => {
-        res.should.have.status(200);
+        res.status.should.equal(201);
+        res.body.message.should.equal('Signed up successfully');
         done();
       });
   });
-  it('should not get access to all books without a token', (done) => {
-    chai.request(Server)
-      .get('/api/v1/books')
+
+  it('Checks for existing username', (done) => {
+    server
+      .post('/api/v1/users/signup')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(seeder.existingUsername)
+      .expect(400)
       .end((err, res) => {
-        res.should.have.status(401);
+        res.status.should.equal(400);
+        res.body.message.should.equal('Username or email already exists');
         done();
       });
   });
-  /**it('should signup users', (done) => {
-    chai.request(Server)
-      .get('signup')
-      .set('content-type', 'application/x-www-form-urlencoded')
-      .send({ username: 'test', password: 'testpassword', email: 'testemail' })
+
+  it('Logs the user in successfully', (done) => {
+    server
+      .post('/api/v1/users/signin')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(seeder.login)
+      .expect(200)
       .end((err, res) => {
-        res.should.have.status(200);
+        res.status.should.equal(200);
+        res.body.message.should.equal('Log in successful');
         done();
       });
-  });**/
+  });
 });
