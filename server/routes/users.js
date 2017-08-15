cf2cf0f48d4ffcaff8acdd46fb4c49dcdbaf6072
babadee001 <temidayooyedele300@gmail.com>
2017-08-15 03:03:30 +0100

    use express routing for heroku

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,16 +8,16 @@ dotenv.load();
 const adminRoute = process.env.route;
 const app = express.Router();
 
-app.route('/api/v1/users/signup')
+app.route('/signup')
   .post(Check.validateInput, UsersController.create);
-app.route('/api/v1/users/signin')
+app.route('/signin')
   .post(Check.validateLogin, UsersController.signin);
 app.route(adminRoute)
   .post(Check.validateInput, UsersController.admin);
-app.route('/api/v1/users/:userId/books/:bookId')
+app.route('/:userId/books/:bookId')
   .post(Check.isLoggedIn, BooksController.borrow);
-app.route('/api/v1/users/:userId/books/:bookId')
+app.route('/:userId/books/:bookId')
   .put(Check.isLoggedIn, BooksController.returnBook);
-app.route('/api/v1/users/:userId/books')
+app.route('/:userId/books')
   .get(Check.isLoggedIn, BooksController.showBorrowed);
 export default app;
