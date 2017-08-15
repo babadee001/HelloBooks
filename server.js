9f5b27096b92e3ea8b22507832e51db11a3ac7df
babadee001 <temidayooyedele300@gmail.com>
2017-08-15 03:01:41 +0100

    edit routing for heroku

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import validator from 'express-validator';
-// import UserRouter from './server/routes/users';
-// import BookRouter from './server/routes/books';
+import UserRouter from './server/routes/users';
+import BookRouter from './server/routes/books';
 
 dotenv.load();
 const app = express();
@@ -14,8 +14,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
-// app.use('/api/v1/users', UserRouter);
-// app.use('/api/v1/books', BookRouter);
+app.use('/api/v1/users', UserRouter);
+app.use('/api/v1/books', BookRouter);
 app.get('/api', (req, res) => {
   res.header(200);
   res.send('Welcome to Hello-Books API');
