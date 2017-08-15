ef3f91fe265c761cb5baaad76aa36270854e8691
babadee001 <temidayooyedele300@gmail.com>
2017-08-15 03:03:56 +0100

    use express routing for heroku

--- server/routes/books.js
+++ server/routes/books.js
@@ -4,12 +4,12 @@ import Check from '../helpers/validation';
 
 const app = express.Router();
 // Routes for books api
-app.route('/api/v1/books') // Get all books
+app.route('/books') // Get all books
   .get(Check.isLoggedIn, BooksController.list);
-app.route('/api/v1/books')
+app.route('/books')
   .post(Check.validateBook, Check.isAdmin,
     BooksController.create);
-app.route('/api/v1/books/:bookId')
+app.route('/books/:bookId')
   .put(Check.isAdmin, Check.validateBook,
     BooksController.edit);// modify book
 export default app;
