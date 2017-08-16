f7c05aa5a9d6cedb8be7b1ce4be0a0561391f69e
babadee001 <temidayooyedele300@gmail.com>
2017-08-16 19:22:54 +0100

    edit package for coveralls

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,11 +1,11 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import Server from '../server';
+// import Users from '../server/models';
 
 const should = chai.should();
 
 chai.use(chaiHttp);
-
 
 describe('Test', () => {
   it('should access api end point', (done) => {
@@ -16,22 +16,18 @@ describe('Test', () => {
         done();
       });
   });
-  /**it('should not get access to all books without a token', (done) => {
-    chai.request(Server)
-      .get('/api/v1/books')
+});
+
+/**
+describe('/GET ', () => {
+  it('it should GET a welcome message', (done) => {
+    chai.request(server)
+      .get('/')
       .end((err, res) => {
-        res.should.have.status(401);
+        res.should.have.status(200);
+        res.body.message.should.equal('Welcome!!!!');
         done();
       });
   });
-  it('should signup users', (done) => {
-    chai.request(Server)
-      .get('signup')
-      .set('content-type', 'application/x-www-form-urlencoded')
-      .send({ username: 'test', password: 'testpassword', email: 'testemail' })
-      .end((err, res) => {
-        res.should.have.status(200);
-        done();
-      });
-  });**/
 });
+**/
