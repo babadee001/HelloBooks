6b1883e9bb9661f05a68a20cc1ce69629fb7b98b
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-10-06 17:45:51 +0100

    add profile page to routes

--- client/routes.js
+++ client/routes.js
@@ -4,11 +4,13 @@ import App from './components/App';
 import Home from './components/home';
 import Signup from './components/users/signup';
 import Signin from './components/users/signin';
+import Profile from './components/users/profile';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={Home} />
-        <Route path="/signup" component={Signup} />
+        <Route path='/signup' component={Signup} />
         <Route path='/signin' component={Signin} />
+        <Route path='/profile' component={Profile} />
     </Route>
 )
