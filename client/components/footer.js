6d7dd58f75409bbe8a8253f47fde8363f4e2dccf
babadee <temidayooyedele300@gmail.com>
2017-10-07 17:05:52 +0100

    add bootstrap for fixed/static design

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Footer }  from 'react-materialize';
-// import Check from './components/container.js'
 
 export default class Foot extends Component {
   render() {
     return (
       <div>
-        <Footer className='teal' copyrights="All rIghts reserved 2017"
-        moreLinks={
-          <a className="grey-text text-lighten-4 right" href="#!">See map</a>
-        }
-      >
-      </Footer>
+        <footer className="navbar-fixed-bottom text-center">
+        <em>           
+          <p className="fa fa-book"> Just Books!  . . . </p>         
+        </em>       
+      </footer>    
       </div>
     );
   }
--- client/components/users/profile.js
+++ client/components/users/profile.js
@@ -21,48 +21,7 @@ export default class Signup extends Component {
 //   }
   render() {
     return (
-        <div className="row profile">
-		<div className="col-md-3">
-			<div className="profile-sidebar">
-				<div className="profile-usertitle">
-					<div className="profile-usertitle-name">
-						Andela Baby
-					</div>
-					<div className="profile-usertitle-job">
-						Developer
-					</div>
-				</div>
-				<div className="profile-userbuttons">
-					<button type="button" className="btn btn-primary btn-sm">Borrowing history</button>
-					<button type="button" className="btn btn-primary btn-sm">Active books</button>
-				</div>
-				<div className="profile-usermenu">
-					<ul className="nav">
-						<li className="active">
-							<a href="#">
-							<i className="glyphicon glyphicon-home"></i>
-							Overview </a>
-						</li>
-						<li>
-							<a href="#">
-							<i className="glyphicon glyphicon-user"></i>
-							Account Settings </a>
-						</li>
-						<li>
-							<a href="#" target="_blank">
-							<i className="glyphicon glyphicon-ok"></i>
-							Notifications </a>
-						</li>
-						<li>
-							<a href="#">
-							<i className="glyphicon glyphicon-flag"></i>
-							Help </a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-        </div>
+        <h1>Profile</h1>
     );
   }
 }
