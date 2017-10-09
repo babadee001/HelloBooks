f0ca325c2ef6c5017eb0e6e7f26fb0f61ebb8236
babadee <temidayooyedele300@gmail.com>
2017-10-09 09:26:25 +0100

    modify signup action

--- client/actions/signupActions.js
+++ client/actions/signupActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export function userSignupRequest(userData) {
-    return dispatch => {
-        return axios.post('api/v1/users/signup', userData);
-    }
+export default function userSignupRequest(userData) {
+  return dispatch =>  {
+    return axios.post('api/v1/users/signup', userData);
+  };
 }
