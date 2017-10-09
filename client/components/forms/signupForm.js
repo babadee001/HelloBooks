fcd7a24d12c62e659a7209a7057276697038e861
babadee <temidayooyedele300@gmail.com>
2017-10-09 09:34:35 +0100

    add client side validation to signup form

--- client/components/forms/signupForm.js
+++ client/components/forms/signupForm.js
@@ -1,45 +1,67 @@
 import React, { Component } from 'react';
+// import classnames from 'classnames';
+import validateInput from './validation/validate';
 
 export default class Signupform extends Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        username: '',
-        password: '',
-        email: ''
-      }
-      this.onChange = this.onChange.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+      confirmPassword: '',
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  isValid() {
+    const { errors, isValid } = validateInput(this.state);
+    if (!isValid) {
+      this.setState({ errors });
     }
-    onChange(e) {
-      this.setState({ [e.target.name]: e.target.value });
-    }
-    onSubmit(e) {
-      e.preventDefault();
-      this.props.userSignupRequest(this.state);
-      // console.log(this.state);
-    }
-    render() {
-      return (
-        <form onSubmit={this.onSubmit}>
-          <h3 className='text-center'>Signup</h3>
-          <div className='form-group'>
-            <label className='control-label'>Username</label>
-            <input type='text' name='username' className='form-control' value={this.state.username} onChange={this.onChange} />
-  
-            <label className='control-label'>Email</label>
-            <input type='text' name='email' className='form-control' value={this.state.email} onChange={this.onChange} />
-  
-            <label className='control-label'>Password</label>
-            <input type='text' name='password' className='form-control' value={this.state.password} onChange={this.onChange} />
-          </div>
-          <div className='form-group'>
-            <button className='btn btn-primary btn-lg'>Sign up </button>
-          </div>
-        </form>
+    return isValid;
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    if (this.isValid()) {
+      this.setState({ errors: {}, isLoading: true });
+      this.props.userSignupRequest(this.state).then(
+        () => {},
+        ({ data }) => this.setState({ errors: data, isLoading: false }),
       );
     }
+    // console.log(this.state);
   }
-  Signupform.propTypes = {
-    userSignupRequest: React.PropTypes.func.isRequired
+
+  render() {
+    return (
+      <form className='' onSubmit={this.onSubmit}>
+        <h3 className='text-center'>Signup</h3>
+        <div className='form-group'>
+          <label className='control-label'>Username</label>
+          <input type='text' name='username' className='form-control' value={this.state.username} onChange={this.onChange} />
+
+          <label className='control-label'>Email</label>
+          <input type='text' name='email' className='form-control' value={this.state.email} onChange={this.onChange} />
+
+          <label className='control-label'>Password</label>
+          <input type='password' name='password' className='form-control' value={this.state.password} onChange={this.onChange} />
+
+          <label className='control-label'>Confirm password</label>
+          <input type='password' name='confirmPassword' className='form-control' value={this.state.confirmPassword} onChange={this.onChange} />
+        </div>
+        <div className='form-group'>
+          <button className='btn btn-primary btn-lg'>Sign up </button>
+        </div>
+      </form>
+    );
   }
+}
+Signupform.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired,
+};
