8b11d5a2ef9708a29ac48edaa9e7ad5235907502
babadee <temidayooyedele300@gmail.com>
2017-11-14 22:50:13 +0100

    feature(signin)
    implement signin feature for users
    
    [Finishes #149782132]

---
+++ client/components/forms/signinForm.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { userSigninRequest } from '../../actions/signupActions';
+
+export default class SigninForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.userSigninRequest(this.state).then(
+      () => {
+        this.context.router.push('/');
+      },
+    );
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4 col-md-offset-4 authenticationWrapper">
+            <div className="authenticationBox">
+              <h4 className="text-center">SIGNIN</h4>
+              <form className="glyphicon" onSubmit={this.onSubmit}>
+                <div className="form-group input-group">
+                  <span className="input-group-addon">
+                    <i className="glyphicon glyphicon-user" />
+                  </span>
+                  <input
+                    className="form-control" value={this.state.username} onChange={this.onChange} type="text"
+                    name="username"
+                    placeholder="Username"
+                    required
+                  />
+                </div>
+                <div className="form-group input-group">
+                  <span className="input-group-addon">
+                    <i className="glyphicon glyphicon-lock" />
+                  </span>
+                  <input
+                    className="form-control" value={this.state.password} onChange={this.onChange} type="password"
+                    name="password" placeholder="Password"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <button className="btn btn-success btn-block">Signin</button>
+                </div>
+                <div className="form-group text-center">
+                Don't have an account?
+                  <a href="signup">Sign up</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+SigninForm.propTypes = {
+  userSigninRequest: React.PropTypes.func.isRequired,
+};
+SigninForm.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+};
+
---
+++ client/components/users/signin.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import SigninForm from '../forms/signinForm';
+import { userSigninRequest } from '../../actions/signupActions';
+
+class Signin extends Component {
+  render() {
+    const { userSigninRequest } = this.props;
+    return (
+      <div>
+        <SigninForm userSigninRequest={ userSigninRequest } />
+      </div>
+    );
+  }
+}
+
+Signin.propTypes = {
+  userSigninRequest: React.PropTypes.func.isRequired,
+};
+
+export default connect((state) => ({}), { userSigninRequest })(Signin);
